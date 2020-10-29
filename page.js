1932343e3d0f2c53156a2dc89007f72975de4d2b
Priya-145 <priya.semicolon@gmail.com>
2020-10-29 13:06:00 +0530

    fonts

---
+++ page.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class page extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={{fontFamily:'Poppins-Bold'}}> textInComponent </Text>
+            </View>
+        )
+    }
+}
