35dc9dde161ffd28b0fb25bfec6b43203ea965e8
MittalSavaliya <72917440+MittalSavaliya@users.noreply.github.com>
2020-10-29 17:18:56 +0530

    first

--- src/screen/signin/sign_in.js
+++ src/screen/signin/sign_in.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import styles from './signin_style'
 
 export default class sign_in extends Component {
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View style={styles.main}>
+                <View style={styles.flex1}>
+                    <Text>sdfgh</Text>
+                </View>
+                <View style={styles.flex2}>
+                    <Text style={styles.text1}>SIGN IN</Text>
+                    <Text style={styles.text2}>Sign in to Continue</Text>
+                </View>
             </View>
         )
     }
---
+++ src/screen/signin/signin_style.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create(
+    {
+        main: {
+            flex: 1,
+            backgroundColor: '#212121',
+        },
+        flex1: {
+            flex: 3,
+
+        },
+        flex2: {
+            flex: 5,
+            backgroundColor: '#000000',
+            borderTopLeftRadius: 25,
+            borderTopRightRadius: 25
+        },
+        text1: {
+            alignSelf: 'center',
+            color:'grey',
+            fontSize:24
+        },
+        text2: {
+
+        }
+
+
+    }
+)
