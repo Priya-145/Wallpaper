1eaf7d7def660c4e55af3866e425648d62dc9cec
Priya-145 <priya.semicolon@gmail.com>
2020-10-29 17:33:33 +0530

    scr

---
+++ src/screen/resetPassword/resetPassword.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import  Styles from '../resetPassword/resetPasswordStyle'
+
+export default class resetPassword extends Component {
+    render() {
+        return (
+            <View style={{backgroundColor:'#161616'}}>
+                <View style={Styles.head}>
+                    <Text style={{color:'white'}}>fghjkl</Text>
+                </View>
+                <View style={Styles.resetView}>
+
+                </View>
+            </View>
+        )
+    }
+}
---
+++ src/screen/resetPassword/resetPasswordStyle.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native'
+import {heightPercentageToDP as hp , widthPercentageToDP as wp} from 'react-native-responsive-screen'
+
+export default StyleSheet.create({
+    head:{
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#161616',
+        height:hp(35.34),
+        width:wp(100)
+    },
+    resetView:{
+        height:hp(64.66),
+        width:wp(100),
+        backgroundColor:'black',
+        borderRadius:30
+    }
+})
