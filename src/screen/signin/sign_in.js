080b70197b6a7a377135102fa7adf9e079afefab
MittalSavaliya <72917440+MittalSavaliya@users.noreply.github.com>
2020-10-30 16:23:54 +0530

    design

--- src/screen/signin/sign_in.js
+++ src/screen/signin/sign_in.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View ,TouchableOpacity,Image} from 'react-native'
 import styles from './signin_style'
 
 export default class sign_in extends Component {
@@ -7,11 +7,21 @@ export default class sign_in extends Component {
         return (
             <View style={styles.main}>
                 <View style={styles.flex1}>
-                    <Text>sdfgh</Text>
+                    <Image source={require('./../../../asset/image/icons/logo-colored.png')} style={styles.logo}></Image>
                 </View>
                 <View style={styles.flex2}>
                     <Text style={styles.text1}>SIGN IN</Text>
                     <Text style={styles.text2}>Sign in to Continue</Text>
+                  
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.text5}>Sign in</Text>
+                    </TouchableOpacity>
+                    <View style={styles.text3}>
+                    <Text style={styles.text6}>Don't have an account?</Text>
+                    <TouchableOpacity style={styles.text4}>
+                        <Text style={styles.text7}>Signup</Text>
+                    </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         )
--- src/screen/signin/signin_style.js
+++ src/screen/signin/signin_style.js
@@ -1,30 +1,62 @@
 import React, { Component } from 'react'
 import { StyleSheet } from 'react-native'
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
+
 
 export default StyleSheet.create(
     {
         main: {
-            flex: 1,
-            backgroundColor: '#212121',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#161616',
         },
         flex1: {
-            flex: 3,
-
+            justifyContent:'center',
+            alignItems:'center',
+            height: hp(35.34),
+            width: wp(100)
         },
         flex2: {
-            flex: 5,
+            height: hp(64.65),
+            width: wp(100),
             backgroundColor: '#000000',
             borderTopLeftRadius: 25,
             borderTopRightRadius: 25
         },
         text1: {
             alignSelf: 'center',
-            color:'grey',
-            fontSize:24
+            color: '#707070',
+            fontSize: 24,
+            // marginTop:7.16
         },
         text2: {
-
-        }
+            alignSelf: 'center',
+            fontSize: 14,
+            color: '#707070',
+            // marginTop:2.47
+        },
+        button: {
+            color: '#707070'
+        },
+        text3: {
+            flexDirection: 'row'
+        },
+        text4: {
+            color: '#707070',
+        },
+        text5: {
+            color: '#707070'
+        },
+        text6:
+        {
+            color: 'red',
+            // marginTop:93.84
+        },
+        text7:
+        {
+            color: 'red'
+        },
+        
 
 
     }
