b7f59de88fe3fa790154827c5a7b63d62ad91017
MittalSavaliya <72917440+MittalSavaliya@users.noreply.github.com>
2020-10-31 14:47:01 +0530

    signin

--- App.js
+++ App.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react'
 
 import Signup from './src/screen/signup/sign_up'
 import { Text, View } from 'react-native'
-import Signin from './src/screen/signin/sign_in'
+// import Signin from './src/screen/signin/sign_in'
 
 // import TextComponet from './src/screen/resetPassword/resetPassword'
 // import Page from './src/screen/resetPassword/resetPassword'
 // import Page from './src/screen/verifycode/verifyCode'
-import In from './src/screen/component/customInput'
+// import In from './src/screen/component/customInput'
 // import Page from './src/screen/resetPassword/resetPassword'
-import Page from './src/screen/latest/latestScreen'
+// import Page from './src/screen/latest/latestScreen'
 
 
 export default class App extends Component {
   render() {
     return (
       
-       <Signin/>
+      //  <Signin/>
     
       // <TextComponet/>
 // import Page from './src/screen/resetPassword/resetPassword'
@@ -28,7 +28,7 @@ export default class App extends Component {
 //     return (
 //       <Page/>
 //     // <In/> 
-    //  <Signup/>
+     <Signup/>
   //   <View>
   // <Text>dsfghjkl</Text>
   //   </View>
